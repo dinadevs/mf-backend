0b19049e37498b185de7e44cf2a8ceb5601675d4
Edna Martins <ednamartins.work@gmail.com>
2025-10-17 11:12:57 -0300

    feat: Create class

---
+++ mf-backend/Models/Vehicle.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mf_backend.Models
+{
+    [Table("Vehicles")]
+    public class Vehicle
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Model Year is required")]
+        public string ModelYear { get; set; }
+
+        [Required(ErrorMessage = "Plate is required")]
+
+        public string Plate { get; set; }
+
+        [Required(ErrorMessage = "Year is required")]
+        public int Year { get; set; }
+
+        [Required(ErrorMessage = "Factory Year is required")]
+        public string FactoryYear { get; set; }
+    }
+}
